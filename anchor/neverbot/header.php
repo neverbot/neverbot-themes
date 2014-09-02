837ffe06e940fe5ee0406ff806946fa5006b71f8
Iván Alonso <neverbot@neverbot.com>
2014-09-03 00:45:02 +0200

    General styling and basic post list

--- anchor/neverbot/header.php
+++ anchor/neverbot/header.php
@@ -1,86 +1,75 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title><?php echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?></title>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo page_title('Page can’t be found'); ?> - <?php echo site_name(); ?></title>
 
-		<meta name="description" content="<?php echo site_description(); ?>">
+    <meta name="description" content="<?php echo site_description(); ?>">
+    <meta name="viewport" content="width=device-width">
+    <meta name="generator" content="Anchor CMS">
 
-		<link rel="stylesheet" href="<?php echo theme_url('/css/reset.css'); ?>">
-		<link rel="stylesheet" href="<?php echo theme_url('/css/style.css'); ?>">
-		<link rel="stylesheet" href="<?php echo theme_url('/css/small.css'); ?>" media="(max-width: 400px)">
+    <meta property="og:title" content="<?php echo site_name(); ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?php echo e(current_url()); ?>">
+    <meta property="og:image" content="<?php echo theme_url('img/og_image.gif'); ?>">
+    <meta property="og:site_name" content="<?php echo site_name(); ?>">
+    <meta property="og:description" content="<?php echo site_description(); ?>">
 
-		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://feeds.feedburner.com/neverbot">
-		<link rel="shortcut icon" href="<?php echo theme_url('img/favicon.ico'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="http://feeds.feedburner.com/neverbot">
+    <link rel="shortcut icon" href="<?php echo theme_url('img/favicon.ico'); ?>">
 
-		<!--[if lt IE 9]>
-			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+    <script>var base = '<?php echo theme_url(); ?>';</script>
 
-		<script>var base = '<?php echo theme_url(); ?>';</script>
-		<script src="<?php echo asset_url('/js/zepto.js'); ?>"></script>
-		<script src="<?php echo theme_url('/js/main.js'); ?>"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-	    <meta name="viewport" content="width=device-width">
-	    <meta name="generator" content="Anchor CMS">
+    <link rel="stylesheet" href="<?php echo theme_url('/css/style.css'); ?>">
 
-	    <meta property="og:title" content="<?php echo site_name(); ?>">
-	    <meta property="og:type" content="website">
-	    <meta property="og:url" content="<?php echo e(current_url()); ?>">
-	    <meta property="og:image" content="<?php echo theme_url('img/og_image.gif'); ?>">
-	    <meta property="og:site_name" content="<?php echo site_name(); ?>">
-	    <meta property="og:description" content="<?php echo site_description(); ?>">
+    <?php if(customised()): ?>
+        <!-- Custom CSS -->
+        <style><?php echo article_css(); ?></style>
 
-		<?php if(customised()): ?>
-		    <!-- Custom CSS -->
-    		<style><?php echo article_css(); ?></style>
+        <!--  Custom Javascript -->
+        <script><?php echo article_js(); ?></script>
+    <?php endif; ?>
 
-    		<!--  Custom Javascript -->
-    		<script><?php echo article_js(); ?></script>
-		<?php endif; ?>
-	</head>
-	<body class="<?php echo body_class(); ?>">
-		<div class="main-wrap">
-			<div class="slidey" id="tray">
-				<div class="wrap">
-					<form id="search" action="<?php echo search_url(); ?>" method="post">
-						<label for="term">Search my blog:</label>
-						<input type="search" id="term" name="term" placeholder="To search, type and hit enter&hellip;" value="<?php echo search_term(); ?>">
-					</form>
+    <?php setlocale(LC_ALL, 'es_ES'); ?>
 
-					<aside>
-						<b>Categories</b>
-						<ul>
-						<?php while(categories()): 
-								if (category_count() > 0) { ?>
-							<li>
-								<a href="<?php echo category_url(); ?>" title="<?php echo category_description(); ?>">
-									<?php echo category_title(); ?> <span><?php echo category_count(); ?></span>
-								</a>
-							</li>
-						<?php } endwhile; ?>
-						</ul>
-					</aside>
-				</div>
-			</div>
+  </head>
+  
+  <body>
+  
+    <header>
+			<nav class="navbar navbar-default navbar-static-top nb-navbar" role="navigation">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsed-nav-menu">
+              <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand nb-logo" href="<?php echo base_url(); ?>">neverbot.com</a>
+          </div>
 
-			<header id="top">
-				<a id="logo" href="<?php echo base_url(); ?>"><?php echo site_name(); ?></a>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="collapsed-nav-menu">
+            <form class="navbar-form navbar-right" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="buscar">
+              </div>
+              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            </form>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
 
-				<nav id="main" role="navigation">
-					<ul>
-						<?php if(has_menu_items()):
-							while(menu_items()): ?>
-						<li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
-							<a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
-								<?php echo menu_name(); ?>
-							</a>
-						</li>
-						<?php endwhile;
-							endif; ?>
-						<li class="tray">
-							<a href="#tray" class="linky"><img src="<?php echo theme_url('img/categories.png'); ?>" alt="Categories" title="View my posts by category"></a>
-						</li>
-					</ul>
-				</nav>
-			</header>
+
+      <div class="nb-head hidden-xs">
+        <a class="nb-main-logo" href="<?php echo base_url(); ?>">neverbot</a>
+      </div>
+
+    </header>
+
+
+
--- anchor/neverbot/posts.php
+++ anchor/neverbot/posts.php
@@ -1,32 +1,26 @@
 <?php theme_include('header'); ?>
 
-<section class="content">
+<section class="nb-content">
 
 	<?php if(has_posts()): ?>
-		<ul class="items">
-			<?php posts(); ?>
+		<ul class="nb-items">
+			<?php $i = 0; while(posts()): ?>
 			<li>
-				<article class="wrap">
-					<h1>
-						<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-					</h1>
+				<article class="nb-post">
+					<header>
+						<h1>
+							<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
+						</h1>
+						<small>Publicado el <?php echo utf8_encode(strftime('%A %e de %B de %Y', article_time())); ?> por <a href="<?php echo base_url(); ?>"><?php echo article_author('real_name'); ?></a></small>
+					</header>
 
 					<div class="content">
 						<?php echo article_markdown(); ?>
 					</div>
 
 					<footer>
-						Posted <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?>.
+						<small>Categoría: <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a> | <a href="<?php echo article_url(); ?>">#</a></small>
 					</footer>
-				</article>
-			</li>
-			<?php $i = 0; while(posts()): ?>
-			<?php $bg = sprintf('background: hsl(215, 28%%, %d%%);', round(((++$i / posts_per_page()) * 20) + 20)); ?>
-			<li style="<?php echo $bg; ?>">
-				<article class="wrap">
-					<h2>
-						<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-					</h2>
 				</article>
 			</li>
 			<?php endwhile; ?>
