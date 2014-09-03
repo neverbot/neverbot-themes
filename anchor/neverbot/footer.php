387da8192ff9f09330700b12adac249c8233060e
Iván Alonso <neverbot@neverbot.com>
2014-09-03 23:57:54 +0200

    Working in the footer

--- anchor/neverbot/footer.php
+++ anchor/neverbot/footer.php
@@ -1,20 +1,44 @@
-		<div class="wrap">
-	            <footer id="bottom">
-	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
 
-	                <ul role="navigation">
-	                    <li><a href="http://feeds.feedburner.com/neverbot">RSS</a></li>
-	                    <?php if(twitter_account()): ?>
-	                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-	                    <?php endif; ?>
+      <footer class="nb-footer">
 
-	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+				 <div class="sidebar-columns">
+				    <div class="sidebar-left">
 
-	                    <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
-	                </ul>
-	            </footer>
+				      <div><i class="fa fa-list-ul"></i>&nbsp;&nbsp;<strong>Categor&iacute;as:</strong></div>
+				      <ul>
+					      <?php $i = 0; while(categories()): ?>
+					      <li>
+					      	<i class="fa fa-caret-right"></i>&nbsp;&nbsp;<a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a> 
+					      </li>
+					      <?php endwhile; ?>
+					    </ul>
 
-	        </div>
-        </div>
-    </body>
+
+				    </div>
+
+				    <div class="sidebar-right">
+
+				    	// pagina about me
+
+				    </div>			  
+			    </div>			  
+
+
+
+
+          <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+
+          <ul role="navigation">
+              <li><a href="http://feeds.feedburner.com/neverbot">RSS</a></li>
+              <?php if(twitter_account()): ?>
+              <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
+              <?php endif; ?>
+
+              <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+
+              <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
+          </ul>
+      </footer>
+
+  </body>
 </html>
--- anchor/neverbot/posts.php
+++ anchor/neverbot/posts.php
@@ -2,46 +2,44 @@
 
 <section class="nb-content">
 
-	<?php if(has_posts()): ?>
-		<ul class="nb-items">
-			<?php $i = 0; while(posts()): ?>
-			<li>
-				<article class="nb-post">
-					<header>
-						<h1>
-							<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-						</h1>
-						<small>Publicado el <?php echo utf8_encode(strftime('%A %e de %B de %Y', article_time())); ?> por <a href="<?php echo base_url(); ?>"><?php echo article_author('real_name'); ?></a></small>
-					</header>
+  <?php if(has_posts()): ?>
+    <ul class="nb-items">
+      <?php $i = 0; while(posts()): ?>
+      <li>
+        <article class="nb-post">
+          <header>
+            <h1>
+              <a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
+            </h1>
+            <small>Publicado el <?php echo utf8_encode(strftime('%A %e de %B de %Y', article_time())); ?> por <a href="<?php echo base_url(); ?>"><?php echo article_author('real_name'); ?></a></small>
+          </header>
 
-					<div class="content">
-						<?php echo article_markdown(); ?>
-					</div>
+          <div class="content">
+            <?php echo article_markdown(); ?>
+          </div>
 
-					<footer>
-						<small>Categoría: <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a> | <a href="<?php echo article_url(); ?>">#</a></small>
-					</footer>
-				</article>
-			</li>
-			<?php endwhile; ?>
-		</ul>
+          <footer>
+            <small>Categoría: <a href="<?php echo article_category_url(); ?>"><?php echo article_category(); ?></a> | <a href="<?php echo article_url(); ?>">#</a></small>
+          </footer>
+        </article>
+      </li>
+      <?php endwhile; ?>
+    </ul>
 
-		<?php if(has_pagination()): ?>
-		<nav class="pagination">
-			<div class="wrap">
-				<div class="previous">
-					<?php echo posts_prev(); ?>
-				</div>
-				<div class="next">
-					<?php echo posts_next(); ?>
-				</div>
-			</div>
-		</nav>
-		<?php endif; ?>
+    <?php if(has_pagination()): ?>
+    <div class="container nb-pagination">
+      <div class="previous">
+        <?php echo posts_prev('&larr; Entradas más antiguas'); ?>
+      </div>
+      <div class="next">
+        <?php echo posts_next('Entradas más modernas &rarr;'); ?>
+      </div>
+    </div>
+    <?php endif; ?>
 
-	<?php else: ?>
-		<p>Looks like you have some writing to do!</p>
-	<?php endif; ?>
+  <?php else: ?>
+    <p>Looks like you have some writing to do!</p>
+  <?php endif; ?>
 
 </section>
 
