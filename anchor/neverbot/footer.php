546307230c140dd9e15e9f2c2b6b45760cfa73ec
Iván Alonso <neverbot@neverbot.com>
2014-09-04 01:42:19 +0200

    footer done

--- anchor/neverbot/footer.php
+++ anchor/neverbot/footer.php
@@ -1,44 +1,49 @@
 
-      <footer class="nb-footer">
+    <footer class="nb-footer">
 
-				 <div class="sidebar-columns">
-				    <div class="sidebar-left">
+		  <div class="sidebar-columns">
+		    <div class="sidebar-left">
+		      <div><i class="fa fa-list-ul"></i>&nbsp;&nbsp;<strong>Categor&iacute;as:</strong></div>
+		      <ul>
+			      <?php $i = 0; while(categories()): ?>
+			      <li>
+			      	<i class="fa fa-caret-right"></i>&nbsp;&nbsp;<a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a> 
+			      	<?php
+			      		if (category_count() > 0)
+			      			echo ' ('.category_count().')';
+			      	?>
+			      </li>
+			      <?php endwhile; ?>
+			    </ul>
+		    </div>
 
-				      <div><i class="fa fa-list-ul"></i>&nbsp;&nbsp;<strong>Categor&iacute;as:</strong></div>
-				      <ul>
-					      <?php $i = 0; while(categories()): ?>
-					      <li>
-					      	<i class="fa fa-caret-right"></i>&nbsp;&nbsp;<a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a> 
-					      </li>
-					      <?php endwhile; ?>
-					    </ul>
+		    <div class="sidebar-right">
+		    	<?php 
+						$page = Page::where('id', '=', '1')->get()[0];
 
+		        // register single post
+		        Registry::set('page', $page);
 
-				    </div>
+		        echo page_content();
+		    	?>
+		    </div>			  
+	    </div>			  
 
-				    <div class="sidebar-right">
+    </footer>
 
-				    	// pagina about me
-
-				    </div>			  
-			    </div>			  
-
-
-
-
-          <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
-
-          <ul role="navigation">
-              <li><a href="http://feeds.feedburner.com/neverbot">RSS</a></li>
-              <?php if(twitter_account()): ?>
-              <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-              <?php endif; ?>
-
-              <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
-
-              <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
-          </ul>
-      </footer>
+    <div class="nb-colophon">
+			<div>
+			  neverbot.com proudly made on Earth
+			  using <a href="http://www.anchorcms.com/">Anchor</a>
+			</div>
+			<div>
+        <a href="http://feeds.feedburner.com/neverbot">rss</a> | 
+        <a href="http://twitter.com/neverbot">@neverbot</a>
+			</div>
+			<div>
+			  4 8 15 16 23 42
+			</div>
+		</div>
 
   </body>
 </html>
