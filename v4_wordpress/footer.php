75af62e82007eb998de90b2aa95f8e10aaf7f94d
Iván Alonso <neverbot@neverbot.com>
2015-02-22 22:06:10 +0100

    Feed link fixed and google analytics script added

--- v4_wordpress/footer.php
+++ v4_wordpress/footer.php
@@ -10,8 +10,8 @@
     using <a href="http://www.wordpress.org/">WordPress</a>
   </div>
   <div>
-    <a href="feed:<?php bloginfo('rss2_url'); ?>">Entradas (feed rss)</a>
-    <a href="feed:<?php bloginfo('rss2_url'); ?>" class="no-a">
+    <a href="<?php bloginfo('rss2_url'); ?>">Entradas (feed rss)</a>
+    <a href="<?php bloginfo('rss2_url'); ?>" class="no-a">
 	  <img src="<?php echo get_template_directory_uri().'/img/feed.png'; ?>" alt="Rss" title="Rss" /></a> 
   </div>
   <div>
@@ -19,6 +19,18 @@
   </div>
 
 </footer>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1007410-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <!-- Theme and design by Ivan Alonso, www.neverbot.com -->
 <?php wp_footer(); ?>
